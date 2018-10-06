a27e3ca66e3b0ff75224d8959a7a32abc5f43dd3
Winston <shuwh317@gmail.com>
2018-10-05 17:34:54 -0700

    add sideDrawer with logo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -10,11 +10,6 @@ const modal = (props) => {
         <Aux>
             <Backdrop show={props.show} clicked={props.clicked}/>
             <div
-                // style={{
-                //     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                //     opacity: props.show ? '1' : '0'
-
-                // }}
                 className={style.join(' ')}>
                 {props.children}
             </div>
