47cc28637cbd8bd7f6f328f45aac0e2a657ea485
Winston <shuwh317@gmail.com>
2018-10-06 00:37:00 -0700

    add backdrop for sideDrawer but has a error

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SiderDrawer';
 
 import classes from './Layout.css';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+// const layout = (props) => (
+//     <Aux>
+//         <Toolbar />
+//         <SideDrawer />
+//         <main className={classes.Content}>
+//             {props.children}
+//         </main>
+//     </Aux>
+// );
 
-export default layout;
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    closeSideDrawerHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    };
+    
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.closeSideDrawerHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    };
+}
+
+export default Layout;
