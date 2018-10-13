32a33ac89a8a6b2bb893a3994ae475250e6674cb
Winston <shuwh317@gmail.com>
2018-10-13 11:04:56 -0700

    add Navlink to navigation bars

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Order from '../../components/Order/Order';
+
+const Orders = (props) => {
+    return (
+        <div>
+            <Order />
+            <Order />
+        </div>
+    );
+}
+
+export default Orders;
+
