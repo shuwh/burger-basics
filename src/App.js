34b9e7b2ce512ef4b855c261291ae0e35cd9a875
Winston <shuwh317@gmail.com>
2018-11-01 13:32:00 -0700

    add lazy loading to dynamically load component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+import asynComponent from './hoc/asynComponent/asynComponent';
+
+const asynAuth = asynComponent(() => import('./containers/Auth/Auth'));
+const asynCheckout = asynComponent(() => import('./containers/Checkout/Checkout'));
+const asynOrders = asynComponent(() => import('./containers/Orders/Orders'));
+const asynLogout = asynComponent(() => import('./containers/Auth/Logout/Logout'));
 
 class App extends Component {
   componentDidMount = () => {
@@ -16,9 +19,10 @@ class App extends Component {
   };
 
   render() {
+    console.log(import('./containers/Auth/Auth'));
     let routes = (
       <Switch>
-        <Route path='/auth' component={Auth} />
+        <Route path='/auth' component={asynAuth} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
@@ -27,10 +31,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/auth' component={Auth} />
+          <Route path='/checkout' component={asynCheckout} />
+          <Route path='/orders' component={asynOrders} />
+          <Route path='/logout' component={asynLogout} />
+          <Route path='/auth' component={asynAuth} />
           <Route path='/' exact component={BurgerBuilder} />
           <Redirect to='/' />
         </Switch>
